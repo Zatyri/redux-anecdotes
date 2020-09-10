73ab1b89f33fcce5e31aa70603b7270de80d2820
Oskar <gustosk@gmail.com>
2020-09-10 20:19:16 +0300

    Tehtävä 6.10
    added notifiacations

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+const initialMessage = 'Welcome to my anecdote site'
+
+
+const reducer = ((state = initialMessage, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+})
+
+export default reducer
